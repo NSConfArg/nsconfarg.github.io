4ecf38fbbc83e31d98718dd2f3e65290261feac5
Carlos Bruscoli <cb@hashboxed.com>
2014-02-17 13:29:23 -0300

    First Push
    Initial Push for landing page

---
+++ Build/send-mail.php
@@ -0,0 +1,22 @@
+<?php
+
+//vars
+$subject = 'Email Confirmacion Nuevo Asistente';
+$to = 'carlosbruscoli@gmail.com';
+
+$from = 'nscfonfarg.com Website';
+
+//data
+$msg .= "EMAIL: "  .$_POST['email']    ."<br>\n";
+
+//Headers
+$headers  = "MIME-Version: 1.0\r\n";
+$headers .= "Content-type: text/html; charset=UTF-8\r\n";
+$headers .= "From: <".$from. ">\r\n" ;
+$headers .= "To: <" . $to . ">";
+
+
+//send for each mail
+var_dump(mail($to, $subject, $msg, $headers));
+
+?>
